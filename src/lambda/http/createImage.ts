39c6101d98f526992017d2f341a2979b4704143f
Nishchay Suteri <nishchay271095@gmail.com>
2021-02-20 23:01:48 +0530

    feat: Implement Create Image API with validation

---
+++ src/lambda/http/createImage.ts
@@ -0,0 +1,74 @@
+import {
+    APIGatewayProxyHandler,
+    APIGatewayProxyEvent,
+    APIGatewayProxyResult,
+} from "aws-lambda";
+import "source-map-support/register";
+import * as AWS from "aws-sdk";
+import * as uuid from "uuid";
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const groupsTable = process.env.GROUPS_TABLE;
+const imagesTable = process.env.IMAGES_TABLE;
+
+export const handler: APIGatewayProxyHandler = async (
+    event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+    console.log("Caller event", event);
+    const groupId = event.pathParameters.groupId;
+    const validGroupId = await groupExists(groupId);
+
+    if (!validGroupId) {
+        return {
+            statusCode: 404,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({
+                error: "Group does not exist",
+            }),
+        };
+    }
+
+    const imageId = uuid.v4();
+    const timestamp = new Date().toISOString();
+    const parsedBody = JSON.parse(event.body);
+
+    const newItem = {
+        groupId: groupId,
+        timestamp: timestamp,
+        imageId: imageId,
+        ...parsedBody,
+    };
+    await docClient
+        .put({
+            TableName: imagesTable,
+            Item: newItem,
+        })
+        .promise();
+
+    return {
+        statusCode: 201,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+            newItem,
+        }),
+    };
+};
+
+async function groupExists(groupId: string) {
+    const result = await docClient
+        .get({
+            TableName: groupsTable,
+            Key: {
+                id: groupId,
+            },
+        })
+        .promise();
+
+    console.log("Get group: ", result);
+    return !!result.Item;
+}
