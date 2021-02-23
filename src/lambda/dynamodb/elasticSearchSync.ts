024cf3bf9088e6f1f365598fe4fe8dd872c0b4dc
Nishchay Suteri <nishchay271095@gmail.com>
2021-02-23 19:13:54 +0530

    feat: Enable DynamoDB Stream

---
+++ src/lambda/dynamodb/elasticSearchSync.ts
@@ -0,0 +1,12 @@
+import { DynamoDBStreamEvent, DynamoDBStreamHandler } from "aws-lambda";
+import "source-map-support/register";
+
+export const handler: DynamoDBStreamHandler = async (
+    event: DynamoDBStreamEvent
+) => {
+    console.log("Processing events batch from DynamoDB", JSON.stringify(event));
+
+    for (const record of event.Records) {
+        console.log("Processing record: ", JSON.stringify(record));
+    }
+};
