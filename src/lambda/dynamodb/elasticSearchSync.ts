486a07803370594be45bdfe3308fe6f9d832b74b
Nishchay Suteri <nishchay271095@gmail.com>
2021-02-23 21:12:25 +0530

    feat: Enable full text search using ElasticSearch

--- src/lambda/dynamodb/elasticSearchSync.ts
+++ src/lambda/dynamodb/elasticSearchSync.ts
@@ -1,5 +1,14 @@
 import { DynamoDBStreamEvent, DynamoDBStreamHandler } from "aws-lambda";
 import "source-map-support/register";
+import * as elasticsearch from "elasticsearch";
+import * as httpAwsEs from "http-aws-es";
+
+const esHost = process.env.ES_ENDPOINT;
+
+const es = new elasticsearch.Client({
+    hosts: [esHost],
+    connectionClass: httpAwsEs,
+});
 
 export const handler: DynamoDBStreamHandler = async (
     event: DynamoDBStreamEvent
@@ -8,5 +17,27 @@ export const handler: DynamoDBStreamHandler = async (
 
     for (const record of event.Records) {
         console.log("Processing record: ", JSON.stringify(record));
+
+        if (record.eventName !== "INSERT") {
+            continue;
+        }
+
+        const newItem = record.dynamodb.NewImage;
+
+        const imageId = newItem.imageId.S;
+        const body = {
+            imageId: imageId,
+            groupId: newItem.groupId.S,
+            imageUrl: newItem.imageUrl.S,
+            title: newItem.title.S,
+            timestamp: newItem.timestamp.S,
+        };
+
+        await es.index({
+            index: "images-index",
+            type: "images",
+            id: imageId,
+            body,
+        });
     }
 };
