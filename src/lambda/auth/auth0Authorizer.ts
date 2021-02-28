47e57bbb769b28833c5902af67f7ec776253537d
Nishchay Suteri <nishchay271095@gmail.com>
2021-02-28 09:12:40 +0530

    feat: Add AWS Secret Manager for Auth0 Secret Key

--- src/lambda/auth/auth0Authorizer.ts
+++ src/lambda/auth/auth0Authorizer.ts
@@ -5,16 +5,24 @@ import {
 } from "aws-lambda";
 import "source-map-support/register";
 
+import * as AWS from "aws-sdk";
+
 import { verify } from "jsonwebtoken";
 import { JwtToken } from "../../auth/jwtToken";
 
-const auth0Secret = process.env.AUTH_0_SECRET;
+const secretId = process.env.AUTH_0_SECRET_ID;
+const secretField = process.env.AUTH_0_SECRET_FIELD;
+
+const client = new AWS.SecretsManager();
+
+// Cache secret if lambda instance is reused
+let cacheSecret: string;
 
 export const handler: CustomAuthorizerHandler = async (
     event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
     try {
-        const decodedToken = verifyToken(event.authorizationToken);
+        const decodedToken = await verifyToken(event.authorizationToken);
         console.log("User was authorized");
         return {
             principalId: decodedToken.sub,
@@ -47,7 +55,7 @@ export const handler: CustomAuthorizerHandler = async (
     }
 };
 
-const verifyToken = (authHeader: string): JwtToken => {
+const verifyToken = async (authHeader: string): Promise<JwtToken> => {
     if (!authHeader) {
         throw new Error("No authorization header");
     }
@@ -57,7 +65,24 @@ const verifyToken = (authHeader: string): JwtToken => {
     const split = authHeader.split(" ");
     const token = split[1];
 
+    const secretObject: any = await getSecret();
+    const auth0Secret = secretObject[secretField];
+
     return verify(token, auth0Secret) as JwtToken;
 
     // a reqquest has been authorized
+};
+
+const getSecret = async () => {
+    if (cacheSecret) return cacheSecret;
+
+    const data = await client
+        .getSecretValue({
+            SecretId: secretId,
+        })
+        .promise();
+
+    cacheSecret = data.SecretString;
+
+    return JSON.parse(cacheSecret);
 };
